0f6ea65cb144efa3a620519c743ea3947d2f9fc9
viennet <viennet@2fb22a62-5409-0410-b6df-a35aca144a8c>
2007-10-09 11:32:15 +0000

    - enhanced set_photo

--- misc/set_photos.py
+++ misc/set_photos.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 """Ajout des photos a posteriori
-   depuis un fichier CSV (avec NIP et photo)
+   depuis un fichier CSV (avec code (etudid ou NIP) et photo)
 """
 
 import pdb,os,sys,psycopg
 import csv
 
 
-CSVFILENAME = '/tmp/trombi-rt1-2007.csv'
+CSVFILENAME = '/tmp/S1.csv'
 #CSVFILENAME = '/tmp/ferhan2.csv'
 
-DBCNXSTRING = 'host=localhost user=scoinfo dbname=SCOI password=XXX'
+DBCNXSTRING = 'host=localhost user=scoinfo dbname=SCOXXX password=XXX'
 
 idx_nip = 0
 idx_photo = 1
 
+code_type = 'etudid'
 
 DO_IT =  False
 #DO_IT = True
@@ -23,23 +24,27 @@ DO_IT =  False
 # en general, pas d'accents dans le CSV
 SCO_ENCODING = 'iso8859-15'
 
-reader = csv.reader(open( CSVFILENAME, "rb"), delimiter=',')
+reader = csv.reader(open( CSVFILENAME, "rb"), delimiter='\t')
 photos = {}
 for row in reader:
     if row[0][0] != '#':
         nip = row[idx_nip]
         if photos.has_key(nip):
             raise ValueError, 'duplicate key: %s' % nip
-        photos[nip] = row[idx_photo]
+        if row[idx_photo]:
+            s = row[idx_photo]
+            s = s.split('.')[0] + '.h90.jpg'
+            photos[nip] = s
 
 cnx = psycopg.connect( DBCNXSTRING )
 
 
 def set_photo(nip, photo): # change toujours
     cursor = cnx.cursor()
-    print "update identite set foto=%(foto)s where code_nip=%(nip)s" % { 'nip':nip, 'foto' : photo }
+    print "update identite set foto=%(foto)s where %(code_type)s=%(code)s" % { 'code_type' : code_type, 'code':nip, 'foto' : photo }
     if DO_IT:
-        cursor.execute("update identite set foto=%(foto)s where code_nip=%(nip)s", { 'nip':nip, 'foto' : photo })
+        r = "update identite set foto=%%(foto)s where %s=%%(code)s" % code_type
+        cursor.execute( r, { 'code':nip, 'foto' : photo })
     return True
 
 
