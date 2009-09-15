741884e4f0b8cec0d06bdd19a6fdd0aad341993a
viennet <viennet@2fb22a62-5409-0410-b6df-a35aca144a8c>
2009-09-15 05:33:30 +0000

    - Fixed upgrade script to ignore nonstandard ScoDoc installation.

--- config/postupgrade.py
+++ config/postupgrade.py
@@ -21,24 +21,33 @@ if os.getuid() != 0:
 
 # Upgrade Apache config to serve Static content (svn 706)
 log('Trying to upgrade your Apache configuration...')
+
+cfg_file = '/etc/apache2/sites-available/scodoc-site-ssl'
 try:
-    cfg_file = '/etc/apache2/sites-available/scodoc-site-ssl'
     txt = open(cfg_file).read()
-    if txt.find('Products/ScoDoc/static') == -1:
-        txt = txt.replace( 'RewriteRule', """# ScoDoc static content, served directly:
+except:
+    txt = None
+    log('*** standard configuration file not found, ignoring')
+    log('%s not found' % cfg_file)
+    log('(you will have to upgrade your Apache configuration by hand)')
+
+if txt:
+    try:
+        if txt.find('Products/ScoDoc/static') == -1:
+            txt = txt.replace( 'RewriteRule', """# ScoDoc static content, served directly:
 RewriteCond %{HTTP:Authorization}  ^(.*)
 RewriteRule ^/ScoDoc/static/(.*) /opt/scodoc/instance/Products/ScoDoc/static/$1 [L]
 
 RewriteRule""", 1)
-        log('Renaming config file %s to %s.bak...' % (cfg_file,cfg_file))
-        os.rename(cfg_file, cfg_file+'.bak')
-        log('Creating new config file...')
-        open(cfg_file,'w').write(txt)
-        log('Restarting Apache...')
-        os.system('/etc/init.d/apache2 restart')
-except:
-    log('\n\n*** Failure ***\n')
-    raise
+            log('Renaming config file %s to %s.bak...' % (cfg_file,cfg_file))
+            os.rename(cfg_file, cfg_file+'.bak')
+            log('Creating new config file...')
+            open(cfg_file,'w').write(txt)
+            log('Restarting Apache...')
+            os.system('/etc/init.d/apache2 restart')
+    except:
+        log('\n\n*** Failure (upgrading Apache configuration) ***\n')
+        raise
 
 # add here actions to perform...
 
