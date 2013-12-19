cf9439bb8f6a5cb429e2939905df82a6d0707b63
eviennet <eviennet@2fb22a62-5409-0410-b6df-a35aca144a8c>
2013-12-19 15:10:02 +0000

    Fix bug: script creation base departement.

--- misc/createtables.sql
+++ misc/createtables.sql
@@ -99,7 +99,7 @@ CREATE TABLE admissions (
     codelycee text,
     debouche text, -- situation APRES etre passe par chez nous (texte libre)
     type_admission text, -- 'APB', 'APC-PC', 'CEF', 'Direct', '?' (autre)
-    boursier_prec integer default NULL, -- etait boursier dans le cycle precedent (lycee) ?
+    boursier_prec integer default NULL -- etait boursier dans le cycle precedent (lycee) ?
 ) WITH OIDS;
 
 CREATE TABLE absences (
